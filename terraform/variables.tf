cf4b44f1542672a2b4ee159bb7b8fc8b49892e6d
Saravanakumaran <60806451+saravanakumaran2@users.noreply.github.com>
2025-08-06 10:44:37 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,8 @@
-variable "public_key" {
-  description = "Public key content from GitHub Secrets"
-  type        = string
+variable "aws_region" {
+  default = "ca-central-1"
 }
 
 variable "key_name" {
-  description = "Name of the EC2 key pair"
+  description = "The name of the existing EC2 key pair"
   type        = string
-}
-
-variable "aws_region" {
-  default = "ca-central-1"
 }
