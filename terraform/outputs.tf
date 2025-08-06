8196928ef14bc0ccb19ea073ff53290b2e03ec2c
Saravanakumaran <60806451+saravanakumaran2@users.noreply.github.com>
2025-08-05 21:31:14 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-  description = "EC2 instance ID"
+  description = "The EC2 instance ID"
   value       = aws_instance.nodejs_app.id
 }
 
-output "instance_public_ip" {
-  description = "Elastic IP address"
+output "public_ip" {
+  description = "The instance's Elastic IP"
   value       = aws_eip.ansible.public_ip
 }
