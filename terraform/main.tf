ce5016cd754ca01d0b2632048092c9a6249d8e1b
Saravanakumaran <60806451+saravanakumaran2@users.noreply.github.com>
2025-08-05 21:31:02 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
@@ -15,11 +15,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
-
 resource "aws_security_group" "node_sg" {
   name   = "node_sg"
   vpc_id = data.aws_vpc.default.id
@@ -29,9 +24,8 @@ resource "aws_security_group" "node_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     description = "HTTP"
     from_port   = 80
@@ -39,12 +33,15 @@ resource "aws_security_group" "node_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -58,11 +55,11 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "nodejs_app" {
-  ami                          = data.aws_ami.ubuntu.id
-  instance_type                = var.instance_type
-  key_name                     = aws_key_pair.deployer.key_name
-  vpc_security_group_ids       = [aws_security_group.node_sg.id]
-  associate_public_ip_address  = false
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.node_sg.id]
+  associate_public_ip_address = false
 
   tags = {
     Name = "nodejs-app"
@@ -70,8 +67,9 @@ resource "aws_instance" "nodejs_app" {
 }
 
 resource "aws_eip" "ansible" {
-  instance    = aws_instance.nodejs_app.id
-  depends_on  = [aws_instance.nodejs_app]
+  instance = aws_instance.nodejs_app.id
+  depends_on = [aws_instance.nodejs_app]
+
   tags = {
     Name = "ansible"
   }
