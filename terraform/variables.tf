a18a7a0201ef660a0b45db9bf904997f0e5a48e7
Saravanakumaran <60806451+saravanakumaran2@users.noreply.github.com>
2025-08-05 21:30:39 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,22 @@
 variable "aws_region" {
   type        = string
   description = "AWS region"
+  default     = "ca-central-1"
 }
+
 variable "key_name" {
   type        = string
+  description = "Name of the AWS key pair"
+  default     = "democentralcanda"
 }
+
 variable "public_key" {
   type        = string
+  description = "Public key material"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type"
+  default     = "t2.micro"
 }
