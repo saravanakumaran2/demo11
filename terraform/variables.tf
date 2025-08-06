3c796c51beb5f1bf93613fb47ac9cdc5ec770f7d
Saravanakumaran <60806451+saravanakumaran2@users.noreply.github.com>
2025-08-05 21:29:41 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,10 @@
 variable "aws_region" {
+  type        = string
   description = "AWS region"
-  type        = string
-  default     = "ca-central-1"
 }
-
 variable "key_name" {
-  description = "Name for AWS key pair"
   type        = string
-  default     = "democentralcanada"
 }
-
-variable "public_key_path" {
-  description = "Path to your public key file"
+variable "public_key" {
   type        = string
-  default     = "~/.ssh/democentralcanda.pub"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "my_ip_cidr" {
-  description = "Your IP for SSH access"
-  type        = string
-  default     = "0.0.0.0/0"
 }
