654f9ce1c3208f8aa8d0c56a8abbe0e66933ef10
Saravanakumaran <60806451+saravanakumaran2@users.noreply.github.com>
2025-08-08 19:41:48 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 80;
-
-app.get('/', (req, res) => res.send('Hello, world! (from EC2)'));
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
