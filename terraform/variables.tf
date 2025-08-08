23634332728f0d6442736678c26f9e99ad2143ad
Saravanakumaran <60806451+saravanakumaran2@users.noreply.github.com>
2025-08-08 19:41:28 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,18 @@ variable "aws_region" {
   default = "ca-central-1"
 }
 
+variable "instance_type" {
+  default = "t2.micro"
+}
+
 variable "key_name" {
-  description = "The name of the existing EC2 key pair"
+  description = "Existing EC2 key pair name"
   type        = string
 }
+
+variable "public_key_path" {
+  description = "Path to the public key file"
+  type        = string
+  default     = ""
+}
+
