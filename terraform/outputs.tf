50e355f72b600bea712b56abb1258ddd45e2c49a
Saravanakumaran <60806451+saravanakumaran2@users.noreply.github.com>
2025-08-08 19:41:11 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "public_ip" {
-  value = aws_instance.nodejs_app.public_ip
+  value = aws_instance.prod_node.public_ip
 }
 
 output "ssh_command" {
-  value = "ssh -i ~/.ssh/democentralcanada.pem ubuntu@${aws_instance.nodejs_app.public_ip}"
+  value = "ssh -i ~/.ssh/democentralcanada.pem ubuntu@${aws_instance.prod_node.public_ip}"
 }
